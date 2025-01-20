1b95a01f016cec7a6034e6c1c84b813ee0a6014c
Dave Dowding <ddowding@sparkgeo.com>
2025-01-20 12:37:51 +0000

    fix: updates to workflow require bucket name rather than arn

--- get_asset_impact.cwl
+++ get_asset_impact.cwl
@@ -32,7 +32,7 @@ $graph:
         NetworkAccess:
             networkAccess: true
         DockerRequirement:
-            dockerPull: public.ecr.aws/z0u8g6n1/eodh:filter02
+            dockerPull: public.ecr.aws/z0u8g6n1/eodh:latest
     baseCommand: get_asset_impact.py
     inputs:
         assets:
--- src/app/get_asset_impact.py
+++ src/app/get_asset_impact.py
@@ -166,11 +166,7 @@ def load_json_from_s3(file_name):
 
     s3 = boto3.client("s3")
     base_name = os.path.basename(file_name)
-    user = file_name.split("/")[0]
-    bucket_arn = (
-        "arn:aws:s3:eu-west-2:312280911266:accesspoint/"
-        f"eodhp-test-gstjkhpo-{user}-s3"
-    )
+    bucket_arn = "workspaces-eodhp-test"
     logger.info(f"Downloading {file_name} from {bucket_arn}...")
     base_name = os.path.basename(file_name)
     s3.download_file(bucket_arn, file_name, base_name)
